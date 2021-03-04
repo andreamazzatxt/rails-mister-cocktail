35734cfe498207131154d55a68a749ca89aadf2d
andreamazzatxt <andreamazza89@gmail.com>
2021-03-04 13:23:37 +0100

    Seed Ingredient

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  validates :description, presence: true
+  belongs_to :ingredient
+  belongs_to :cocktail
+  validates  :ingredient, uniqueness: {
+    scope: :cocktail
+  }
+end
