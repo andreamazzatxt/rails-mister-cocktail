4ef3b4ae4460377cb5efe3c8ae1b1d814181ad04
andreamazzatxt <andreamazza89@gmail.com>
2021-03-05 11:53:30 +0100

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ require 'open-uri'
 require 'json'
 puts 'Start Seeeding!'
 # Seed Ingredients
+Cocktail.destroy_all
 Ingredient.destroy_all
 data = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
 cocktails = JSON.parse(data)
@@ -12,7 +13,6 @@ cocktails['drinks'].each do |ingredient|
 end
 
 # Seed cocktails
-Cocktail.destroy_all
 pictures = %w[
   https://images.unsplash.com/photo-1550426735-c33c7ce414ff?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Y29ja3RhaWx8ZW58MHwxfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60
   https://images.unsplash.com/photo-1551538827-9c037cb4f32a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Y29ja3RhaWx8ZW58MHwxfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60
@@ -39,3 +39,4 @@ pictures.each do |pic|
     Dose.create!(description: dose.sample, ingredient: random_ingredient, cocktail: Cocktail.last)
   end
 end
+puts 'Done !!! 🌱'
